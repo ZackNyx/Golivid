46c7b86b36dde01d9957ab34348db511524f9405
HotPotato451 <91201105+HotPotato451@users.noreply.github.com>
2025-04-10 02:34:41 +0400

    Remove failed attempt to add separate viewport to Giant

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,20 +1,9 @@
 extends Node3D
 
 
-var desktop_viewport: Viewport
-var vr_viewport: Viewport
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     pass
-    #desktop_viewport = %DesktopViewport
-    #vr_viewport = %VRViewport
-    #
-    #var desktop_camera = %PlayerCamera
-    #desktop_viewport.set_camera(desktop_camera)
-    #
-    #var vr_camera = %VRViewport
-    #vr_viewport.set_camera(vr_camera)
     
 
 func reload_current_scene():
@@ -29,15 +18,11 @@ func reload_current_scene():
     var new_scene = load(scene_path).instantiate()
     get_tree().root.add_child(new_scene)
     get_tree().current_scene = new_scene
-    
-    
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
     # Restart functionality
     if Input.is_action_just_pressed('DEBUG_RESTART'):
         reload_current_scene()
-    
-    #desktop_viewport.update()
-    #vr_viewport.update()
     
