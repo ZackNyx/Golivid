850a92e8051d8de12a2f107bb514f81bdccbb1e3
HotPotato451 <91201105+HotPotato451@users.noreply.github.com>
2025-04-10 00:29:13 +0400

    Set up code for separate player viewports

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,9 +1,21 @@
 extends Node3D
 
 
+var desktop_viewport: Viewport
+var vr_viewport: Viewport
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
+    pass
+    #desktop_viewport = %DesktopViewport
+    #vr_viewport = %VRViewport
+    #
+    #var desktop_camera = %PlayerCamera
+    #desktop_viewport.set_camera(desktop_camera)
+    #
+    #var vr_camera = %VRViewport
+    #vr_viewport.set_camera(vr_camera)
+    
 
 func reload_current_scene():
     # Get the current scene's file path
@@ -17,8 +29,15 @@ func reload_current_scene():
     var new_scene = load(scene_path).instantiate()
     get_tree().root.add_child(new_scene)
     get_tree().current_scene = new_scene
+    
+    
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+    # Restart functionality
     if Input.is_action_just_pressed('DEBUG_RESTART'):
         reload_current_scene()
+    
+    #desktop_viewport.update()
+    #vr_viewport.update()
+    
