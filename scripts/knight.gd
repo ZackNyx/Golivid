f32f240f8673a34cf5b7315de4aad70d03dcae4d
HotPotato451 <91201105+HotPotato451@users.noreply.github.com>
2025-04-18 21:59:18 +0400

    RC

--- scripts/knight.gd
+++ scripts/knight.gd
@@ -26,7 +26,6 @@ var is_rolling := false
 @onready var roll_timer: Timer = %RollTimer
 @onready var roll_cooldown: Timer = %RollCooldown
 
-
 func _input(event: InputEvent) -> void:
     if event.is_action_pressed('left_click'):
         Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
